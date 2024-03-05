523a63b2c6917109205e5b34e5dc140b4396bdb4
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-03-05 18:35:55 +0530

    Worked on userProfile view. Displayed user details on user info page.

--- src/components/CustomNavBar.jsx
+++ src/components/CustomNavBar.jsx
@@ -82,7 +82,7 @@ const CustomNavBar = () => {
             {login && (
               <>
                 <NavItem>
-                  <NavLink tag={ReactLink} to="/user/profile-info">
+                  <NavLink tag={ReactLink} to={`/user/profile-info/${user.id}`}>
                     Profile Info
                   </NavLink>
                 </NavItem>
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -4,5 +4,11 @@ export const signUp = (user) => {
   return myAxios.post("/auth/register", user).then((response) => response.data);
 };
 export const login = (loginDetails) => {
-  return myAxios.post("/auth/login", loginDetails).then((response) => response.data);
+  return myAxios
+    .post("/auth/login", loginDetails)
+    .then((response) => response.data);
+};
+
+export const getUser = (userId) => {
+  return myAxios.get(`/users/${userId}`).then((resp) => resp.data);
 };
